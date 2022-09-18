643f7b6f673974a4979e82dc306b3adc870be6c3
Muneeb Nawaz <muneebnawaz97@gmail.com>
2022-09-18 07:17:45 -0700

    debugging routing error on heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,10 @@ class ApplicationController < ActionController::Base
     render file: Rails.public_path.join("404.html"), status: :not_found, layout: false
   end
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
   protected
 
   def configure_permitted_parameters
