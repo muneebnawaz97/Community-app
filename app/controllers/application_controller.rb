9274ae651f71773dc411fbfbedc8c9dca216bb53
Muneeb Nawaz <muneebnawaz97@gmail.com>
2022-09-18 08:48:37 -0700

    Added default sign out path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,10 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
   protected
 
   def configure_permitted_parameters
