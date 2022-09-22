ddbce597a82daef64adbe4fe4ee66eb8939ff396
Muneeb Nawaz <muneebnawaz97@gmail.com>
2022-09-21 17:55:14 -0700

    Removed duplicate code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,7 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-  rescue_from NameError, with: :route_not_found
-  rescue_from ActionController::RoutingError, with: :route_not_found
-  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_response
-
+  
   def after_sign_in_path_for _resource
     root_path
   end
